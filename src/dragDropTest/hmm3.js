408f7d3cf72ed866c37129da8bb54f7bd7b3b610
JMN327 <173676271+JMN327@users.noreply.github.com>
2024-09-30 12:54:51 +0100

    Add snap animation to drag and drop

--- src/dragDropTest/hmm3.js
+++ src/dragDropTest/hmm3.js
@@ -3,6 +3,7 @@ const gridContainer = document.querySelector(".grid-container");
 let row = null;
 let shiftY = null;
 let startY = null;
+let currentYPos = null;
 
 gridContainer.addEventListener("dragstart", (event) => {
   event.preventDefault();
@@ -18,7 +19,9 @@ gridContainer.addEventListener("mousedown", (event) => {
   /* row.style.position = absolute; */
   shiftY = event.offsetY;
   startY = row.getBoundingClientRect().top;
-  console.log(event.offsetY);
+  console.log("StartY: " + startY);
+
+  const fxMove = `transform: translate(0px, 0px);`;
 });
 
 gridContainer.addEventListener("mousemove", (event) => {
@@ -26,7 +29,8 @@ gridContainer.addEventListener("mousemove", (event) => {
     return;
   }
   let mouseYRelToContainer = event.clientY - startY;
-  row.style.top = mouseYRelToContainer - shiftY + "px";
+  currentYPos = mouseYRelToContainer - shiftY;
+  row.style.top = currentYPos + "px";
   console.log(
     "Item: " +
       row.textContent +
@@ -36,7 +40,18 @@ gridContainer.addEventListener("mousemove", (event) => {
 });
 
 gridContainer.addEventListener("mouseup", (event) => {
-  row = null;
-  shiftY = null;
-  startY = null;
+  let endYPos = currentYPos;
+  const snap = [{ transform: `translate(0px, ${-endYPos}px)` }];
+  const snapTiming = {
+    duration: 200,
+    iterations: 1,
+  };
+
+  const snapAni = row.animate(snap, snapTiming);
+  snapAni.onfinish = (event) => {
+    row.style.top = 0 + "px";
+    row = null;
+    shiftY = null;
+    startY = null;
+  };
 });
